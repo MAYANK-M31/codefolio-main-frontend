f8a18d477d9a369b1fb6d8f85928deab0aec976f
MAYANK-M31 <mayank31m@gmail.com>
2022-12-12 03:06:36 +0530

    doing

--- pages/[username].js
+++ pages/[username].js
@@ -2,6 +2,7 @@ import react, { useEffect, useState } from "react";
 import { useRouter } from "next/router";
 import { useQuery } from "react-query";
 import { baseurl } from "../public/baseurl";
+import styles from "../styles/Profile.module.css";
 
 export default function profile() {
   const router = useRouter();
@@ -20,8 +21,38 @@ export default function profile() {
 
   if (error) return console.error(error);
 
+  console.log(data);
+
   return (
-    <div>
+    <div className={styles.container}>
+      <div className={styles.profileDiv}>
+        <div className={styles.profileLeft}>
+          <img
+            src="https://media.geeksforgeeks.org/auth/profile/0qzwgked2yyt9siozgb3"
+            className={styles.profileimg}
+          />
+          <div className={styles.profileTextDiv}>
+            <h1>MAYANK MALHOTRA</h1>
+            <h2>SOFTWARE ENGINEER INTERN</h2>
+            <p>Profiles:</p>
+            <div className={styles.linksDiv}>
+              <button>
+                <img src="/icons/leetcode.png" alt="lc" />
+              </button>
+              <button className="gfgicon">
+                <img src="/icons/gfg.png" alt="GFG" />
+              </button>
+            </div>
+          </div>
+        </div>
+        <div className={styles.profileRight}></div>
+      </div>
+    </div>
+  );
+}
+
+{
+  /* <div>
       <p>{username}</p>
       {!isLoading ? (
         <div>
@@ -31,6 +62,5 @@ export default function profile() {
       ) : (
         <p>LOADING..</p>
       )}
-    </div>
-  );
+    </div> */
 }
--- public/baseurl.js
+++ public/baseurl.js
@@ -1,2 +1,2 @@
-export const baseurl = "https://mayank-m31-stunning-bassoon-g646x9gx5x63v4x5-8000.preview.app.github.dev"
+export const baseurl = "https://mayank-m31-effective-engine-wxrxv6wvr64cggv7-8000.preview.app.github.dev"
 // "http://localhost:8000"
